df5c276102e43ef09bab2080e563a11dd48420eb
sharma90 <sharmaravi9098@gmail.com>
2026-03-28 17:39:47 +0530

    push v3 version changes

--- app/api/user_controller.py
+++ app/api/user_controller.py
@@ -79,6 +79,7 @@ def get_user(
         print("Enter get_user method")
         print("Enter get_user method again change done")
         print("Enter get_user method again change done2222")
+        print("Enter get_user method again change done3333")
         user_data = breaker.call(service.get_user,db, user_id)
         return ApiResponse(success=True, data=user_data)
     except pybreaker.CircuitBreakerError as e:
